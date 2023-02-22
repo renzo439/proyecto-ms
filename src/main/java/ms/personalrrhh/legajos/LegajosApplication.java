645c6aa02953760630fe86b7cd15ee5375a09b43
ren439 <renzoantonio.ramos@alumnos.ucse.edu.ar>
2023-02-22 09:40:22 -0300

    firt commit

---
+++ src/main/java/ms/personalrrhh/legajos/LegajosApplication.java
@@ -0,0 +1,13 @@
+package ms.personalrrhh.legajos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegajosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegajosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/personalrrhh/legajos/LegajosApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.personalrrhh.legajos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegajosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
